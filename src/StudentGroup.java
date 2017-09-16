ada62ef78371eadd162b0969354df06ff16d1e0f
ashok110863 <32007340+ashok110863@users.noreply.github.com>
2017-09-16 12:13:20 +0530

    Last commit

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -1,6 +1,10 @@
 import java.awt.List;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 
@@ -282,21 +286,38 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public int getCurrentAgeByDate(int indexOfStudent) {
-		// Add your implementation here
-		return 0;
+		if(indexOfStudent==0){
+			throw new IllegalArgumentException();
+		}else{
+			Student student=this.students[indexOfStudent];
+			return returnAge(student.getBirthDate());
+		}
 	}
 
 	@Override
 	public Student[] getStudentsByAge(int age) {
 		ArrayList<Student> arr=new ArrayList<Student>();
 		for(int i=0;i<this.students.length;i++){
+			if(age==returnAge(this.students[i].getBirthDate())){
 			arr.add(this.students[i]);
+			}
 		}
 		Student[] newArr = new Student[arr.size()];
 		newArr = arr.toArray(newArr);
 		return newArr;
 	}
 
+	public int returnAge(Date bday){
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(bday);
+		LocalDate birthday = LocalDate.of(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DATE));
+		LocalDate now = LocalDate.of(2016, Month.SEPTEMBER, 16);
+
+		// using period
+		Period period = Period.between(birthday, now);
+		return period.getYears();
+	}
+	
 	@Override
 	public Student[] getStudentsWithMaxAvgMark() {
 		double d = 0;
